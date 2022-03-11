45f21d2152373f1c54ef2bd185693e7cb9b22b1d
Xavier Nodet <xavier.nodet@fr.ibm.com>
2022-03-11 08:39:50 +0100

    Limit inbound traffic as well

--- throttle.sh
+++ throttle.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Max speeds in KB/sec
-OUTBOUND=100
+# Max inbound speeds in KB/sec
+INBOUND=700
+if [ "$1" != "" ]; then
+   INBOUND=$1
+   shift
+fi
 
+# Max outbound speeds in KB/sec
+OUTBOUND=70
 if [ "$1" != "" ]; then
    OUTBOUND=$1
    shift
@@ -30,13 +36,18 @@ no dummynet quick on lo0 all
 dummynet out all pipe 3
 dummynet out to 192.168.0.0/16 pipe 4
 dummynet out proto icmp all pipe 4
+dummynet in all pipe 5
+dummynet in from 192.168.0.0/16 pipe 6
+dummynet in proto icmp all pipe 6
 EOF
 
 # Create the dummynet queue
 dnctl pipe 3 config bw ${OUTBOUND}Kbyte/s queue 50
 dnctl pipe 4 config queue 50
+dnctl pipe 5 config bw ${INBOUND}Kbyte/s queue 50
+dnctl pipe 6 config queue 50
 
 # Activate PF
 pfctl -E
 
-echo "Output limited to $OUTBOUND KB/s"
+echo "Bandwidth limited to $INBOUND/$OUTBOUND KB/s"
